ae339dec81de1fd04dbbb4f6d74d2b134d48d3f7
taeo <xoommnj@gmail.com>
2026-06-10 17:07:01 +0900

    feat: initialize devjarvis monorepo

---
+++ devjarvis-backend/src/test/java/com/taeo/devjarvis/backend/DevjarvisBackendApplicationTests.java
@@ -0,0 +1,13 @@
+package com.taeo.devjarvis.backend;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DevjarvisBackendApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
